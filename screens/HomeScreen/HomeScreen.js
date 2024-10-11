908a5a58dd33a2454bf9245621772883ff62c174
Dawyy I <21045081.y@student.iuh.edu.vn>
2024-10-11 12:38:55 +0700

    Setup project structure, navigation

---
+++ screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const HomeScreen = () => {
+
+
+
+    return(
+        <View>
+            <Text>Home Screen</Text>
+
+        </View>
+    );
+};
+
+export default HomeScreen;
